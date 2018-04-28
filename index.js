008fd4e5e2af7e8ef8dd010337ab1f405afc0d2c
Eric (Martin) Mancini <coelomate@gmail.com>
2018-04-28 13:25:56 -0400

    handlebars example working woo

--- index.js
+++ index.js
@@ -13,26 +13,7 @@ var PORT = process.env.PORT || 4000;
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-//
-// ROUTES
-//
-
-app.get('/', (req, res) => {
-    res.send(`
-    <html>
-        <head>
-        </head>
-        <body>
-            <h1>Welcome to the pre-handlebars test page!</h1>
-            <p><a href="/icecream/chocolate">Chocolate</a></p>
-            <p><a href="/icecream/banana">banana</a></p>
-
-        </body>
-    </html>
-    `);
-});
-
-var icecreams = [
+const ICECREAMS = [
     { name: 'vanilla', price: 10, awesomeness: 3 },
     { name: 'chocolate', price: 4, awesomeness: 8 },
     { name: 'banana', price: 1, awesomeness: 1 },
@@ -40,12 +21,37 @@ var icecreams = [
     { name: 'jawbreakers', price: 6, awesomeness: 2 },
 ];
 
+//
+// ROUTES
+//
+
+// app.get('/', (req, res) => {
+//     res.send(`
+//     <html>
+//         <head>
+//         </head>
+//         <body>
+//             <h1>Welcome to the pre-handlebars test page!</h1>
+//             <p><a href="/icecream/chocolate">Chocolate</a></p>
+//             <p><a href="/icecream/banana">banana</a></p>
+//             <p><a href="/newindex">New Index</a></p>
+//         </body>
+//     </html>
+//     `);
+// });
+
+app.get('/', (req, res) => {
+    res.render("index", {ICECREAMS: ICECREAMS});
+});
+
+
+
 
 app.get("/icecream/:name", function (req, res) {
     var chosen_ice_cream = req.params.name;
-    for (let i = 0; i < icecreams.length; i++) {
-        if (chosen_ice_cream.toLowerCase() === icecreams[i].name) {
-            res.render("icecream", icecreams[i]);
+    for (let i = 0; i < ICECREAMS.length; i++) {
+        if (chosen_ice_cream.toLowerCase() === ICECREAMS[i].name) {
+            res.render("icecream", ICECREAMS[i]);
             return;
         }
     }
